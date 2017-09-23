4bc040de46266d30157f940b4ede575af86ac950
vpavlov <vladislav.m.pavlov@googlemail.com>
2017-09-23 19:52:42 +0200

    add initial version

---
+++ src/PID.cpp
@@ -0,0 +1,39 @@
+#include "PID.h"
+
+using namespace std;
+
+/*
+* TODO: Complete the PID class.
+*/
+
+PID::PID() {}
+
+PID::~PID() {}
+
+void PID::Init(double Kp, double Ki, double Kd) {
+
+    this->Kp = Kp;
+    this->Kd = Kd;
+    this->Ki = Ki;
+
+    p_error = 0.0;
+    d_error = 0.0;
+	i_error = 0.0;
+
+}
+
+void PID::UpdateError(double cte) {
+    
+    // diff-term
+    d_error = cte - p_error;
+    // p-term
+    p_error = cte;
+    // integral-term
+	i_error = i_error + cte;
+}
+
+double PID::TotalError() {
+
+	return -(Kp*p_error + Ki*i_error + Kd*d_error);
+}
+
