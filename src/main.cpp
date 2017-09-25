0627d9bff34b0d7711c5189ef33997bfbde394b4
vpavlov <vladislav.m.pavlov@googlemail.com>
2017-09-25 21:47:22 +0200

    bugfix: switched Kd and Ki

--- src/main.cpp
+++ src/main.cpp
@@ -34,7 +34,7 @@ int main()
 
   PID pid;
   // TODO: Initialize the pid variable.
-  pid.Init(0.3, 5.0, 0.0);
+  pid.Init(0.13, 0.0003, 3.0);
 
   h.onMessage([&pid](uWS::WebSocket<uWS::SERVER> ws, char *data, size_t length, uWS::OpCode opCode) {
     // "42" at the start of the message means there's a websocket message event.
@@ -52,7 +52,7 @@ int main()
           double speed = std::stod(j[1]["speed"].get<std::string>());
           double angle = std::stod(j[1]["steering_angle"].get<std::string>());
           double steer_value;
-          double max_steering = 0.1;
+          double max_steering = 1.0;
           // DEBUG
           std::cout << "speed: " << speed << " angle: " << angle << std::endl;
           /*
